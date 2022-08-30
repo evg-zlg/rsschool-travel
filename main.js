384dccc76667a64153ec947a8bfc8d6fa12ccae6
evg-zlg <evg.zalogin@gmail.com>
2022-08-30 12:55:55 +0500

    feat: start work with login popup

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log('hi');
+document.querySelector(".js-header-button").addEventListener("click", () => {
+  let loginPopUp = document.querySelector(".login-popup");
+  if (loginPopUp.classList.contains("login-popup_show")) {
+    loginPopUp.classList.add("login-popup_show");
+    document.body.style.overflow = "hidden";
+
+  } 
+});
